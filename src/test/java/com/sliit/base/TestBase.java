51b1083b84c3f4f384661eab47e032bc5c8f3c6d
JayashaniG <JayashaniG@99x.lk>
2019-08-21 17:42:17 +0530

    updated xpaths for awarding institutes and added configurations and driver for firefox and edge

--- src/test/java/com/sliit/base/TestBase.java
+++ src/test/java/com/sliit/base/TestBase.java
@@ -141,7 +141,7 @@ public class TestBase {
 
 			if (config.getProperty("browser").equals("firefox")) {
 
-				// System.setProperty("webdriver.gecko.driver", "gecko.exe");
+				System.setProperty("webdriver.gecko.driver", "C:\\Users\\jayashanig\\Documents\\Projects\\SLIIT\\Automation\\SLIIT\\src\\test\\resources\\executables\\geckodriver.exe");
 				driver = new FirefoxDriver();
 
 			} else if (config.getProperty("browser").equals("chrome")) {
@@ -153,7 +153,7 @@ public class TestBase {
 			} else if (config.getProperty("browser").equals("ie")) {
 
 				System.setProperty("webdriver.ie.driver",
-						System.getProperty("user.dir") + "\\src\\test\\resources\\executables\\IEDriverServer.exe");
+						System.getProperty("user.dir") + "\\src\\test\\resources\\executables\\MicrosoftWebDriver.exe");
 				driver = new InternetExplorerDriver();
 
 			}
@@ -971,8 +971,6 @@ public class TestBase {
 
 		verifyContains(message_after_save, "successfully!");
 	}
-	
-	
 	
 	/**
 	 * @author ArshadM 
